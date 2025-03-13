28300bf4ccf43d692047a8847c08c139ee1242b5
TheJoshChambers <Josh.Chambers2@student.shu.ac.uk>
2025-03-13 10:20:12 +0000

    Update server.js
    amazing

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const WebSocket = require('ws');
 const os = require('os');
 
-// temp data will implement grabbing data from db
+// temp data will implement grabbing data from database
 const users = {
     admin: { password: 'adminpass', role: 'admin' },
     user: { password: 'userpass', role: 'user' }
